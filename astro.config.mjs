769f0de135396decd47cee2c54dc171783aafb6f
Varun Sahni <sahnivarun62@gmail.com>
2023-11-25 14:38:10 +0530

    Static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  output: "server",
   adapter: vercel()
 });
