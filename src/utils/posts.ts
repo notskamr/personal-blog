43a866b3362f588bf43c4ce7b9449f80e0ef4bc8
Varun Sahni <sahnivarun62@gmail.com>
2024-04-17 21:00:52 +0530

    Update deps + fix logic for publishing

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -13,15 +13,19 @@ export async function getPosts(limit: number = 4, onlyPublished: boolean = true)
                 filter: {
                     status: {
                         _eq: "published",
-                    },
-                    published_at: {
-                        _lte: new Date().toISOString()
                     }
                 },
                 fields: ["*,seo.*"]
             })
         })
     );
+    // filter blogs that have a published_at date in the future
+    blogs = blogs.filter((blog) => {
+        if (blog.published_at) {
+            return new Date(blog.published_at) <= new Date();
+        }
+        return true;
+    });
     return blogs;
 }
 
